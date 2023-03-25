4f06f80708d255c7815cde74e813df9800e68272
Damon German <damon.german@hyland.com>
2023-03-25 15:22:49 -0500

    made some changes

---
+++ CurrencyConverter/Services/CachingCurrencySymbolService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace CurrencyConverter.Services;
+
+public class CachingCurrencySymbolService : ICurrencySymbolService
+{
+    private readonly ICurrencySymbolService _innerService;
+    private readonly Hashtable _cache;
+
+    public CachingCurrencySymbolService(ICurrencySymbolService innerService)
+    {
+        _innerService = innerService;
+        _cache = new Hashtable();
+    }
+
+    public string[] GetCurrencySymbols()
+    {
+        const string cacheKey = "CurrencySymbols";
+
+        if (!_cache.ContainsKey(cacheKey))
+        {
+            var symbols = _innerService.GetCurrencySymbols();
+            _cache[cacheKey] = symbols;
+        }
+
+        return (string[])_cache[cacheKey];
+    }
+}
